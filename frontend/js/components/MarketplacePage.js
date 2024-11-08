6153b94d594c793125305095feb09f7681895eb8
Winifred Nauda <winifrednauda@gmail.com>
2024-11-08 17:36:33 -0500

    initial design and todos for marketplace page

---
+++ frontend/js/components/MarketplacePage.js
@@ -0,0 +1,34 @@
+// main product list rendering
+// TODO: implement the following:
+
+// list of objects of the form
+// {prodid, imgurl, name, average rating, numreviews, price}
+// eg. {1, https://gravel.png, "Gravel", 3.5, 10, 10.00}, {2, https://kelp.png, "Kelp", null, 0, 19.99}
+// prodid is not displayed, just an internal way to keep track of products
+
+// a default 0, b default 9
+// for (products a - b in product list)
+//      create html to display the product
+
+//           should look something like:
+//           <div class="product">
+//              <div class="prodimg">
+//                  <img src=imgurl></img>
+//              </div>
+//              <div class="proddesc">
+//                  <a href=[link to product page]>name</a>
+//                  <img src=[starratingicon]></img>
+//                  <a href=[link to ratings]>numreviews</a>
+//                  <p>$price</p>
+//              </div>
+//           </div>
+
+//      add to dom
+
+// button to increase a and b by 10 (next page) or decrease a and b by 10 (previous page)
+
+// sort and filter
+// TODO: future feature
+
+// browse categories
+// TODO: future feature
