293caf44881ca2fed4f2e769d197377fe8d907cf
Winifred Nauda <winifrednauda@gmail.com>
2024-11-15 21:22:23 -0500

    implemented filter functionality, refactored rendering the main marketplace list into a separate method

--- frontend/main/components/MarketplacePage/MarketplacePage.js
+++ frontend/main/components/MarketplacePage/MarketplacePage.js
@@ -1,49 +1,108 @@
 import { BaseComponent } from "../../app/BaseComponent.js";
+import { Category } from "../shared/Category.js";
+import { PriceBrackets } from "./PriceBrackets.js";
 
 export class MarketplacePage extends BaseComponent {
-    // main product list rendering
-    // list of products at the top, then buttons to change pages at the bottom.
-
     constructor() {
         super();
         this.container.classList.add('marketplace-page');
-        this.marketplace = document.createElement('div');
-        this.marketplace.classList.add('marketplace');
-        this.fullProdList = this.getProdList;
+        this.fullProdList = this.getProdList();
+        this.marketplace = document.createElement("div");
 
         // will be changed with search/filter, need to keep track of full original list :)
-        this.prodlist = this.fullProdList();
+        this.prodlist = this.fullProdList;
+        this.curCategory = "All";
+        this.curBracket = "All";
 
         this.start = 0, this.end = 5;
     }
 
     getProdList() { // will be async
-        return [
+        const list = [
             {prodid: 1, 
             sellername: "Neuvillette",
             sellerid: 1,
             imgurl: "./assets/dummy_600x400_ffffff_cccccc.png", 
             name: "Water", 
+            category: Category.Misc,
+            bracket: "",
             description: "Water comes in many flavors to the discerning palate. Mondstadt's water is crisp and pure, while water from Liyue has an enduring aftertaste. In Inazuma, the water possesses a depth of flavor unlike any other. Sumeru's water, meanwhile, has a rich and complex flavor profile, but it must be savored patiently to fully appreciate it.",
             average_rating: 3.5, 
             numreviews: 10, 
-            price: 10.00}, 
+            price: 100.00}, 
             {prodid: 2, 
             sellername: "Seller 2",
             sellerid: 2,
             imgurl: "./assets/dummy_600x400_ffffff_cccccc.png", 
             name: "Kelp", 
+            category: Category.Plants,
+            bracket: "",
             description: "Kelp mmm tastey :)",
             average_rating: null, 
             numreviews: 0, 
             price: 19.99}
         ]; // will eventually be fetched from server, this is just test data
+
+        for (let i = 0; i < list.length; i++) {
+            list[i].bracket = this.calculateBracket(list[i].price);
+        }
+
+        return list;
+    }
+
+    calculateBracket(price) {
+        if (price < 20) {
+            return PriceBrackets.bracket1;
+        } else if (price < 50) {
+            return PriceBrackets.bracket2;
+        } else if (price < 100) {
+            return PriceBrackets.bracket3;
+        } else if (price < 500) {
+            return PriceBrackets.bracket4;
+        } else if (price < 1000) {
+            return PriceBrackets.bracket5;
+        } else {
+            return PriceBrackets.bracket6;
+        }
     }
 
     render() {
-        // empty previous render if any
-        this.marketplace.innerHTML = "";
+        const filters = document.createElement("div");
+        filters.classList.add("filters");
+        this.container.appendChild(filters);
 
+        const categoryFilter = document.createElement("div");
+        categoryFilter.classList.add("filter-label");
+        categoryFilter.innerText = "Categories:";
+        filters.appendChild(categoryFilter);
+
+        for (let category in Category) {
+            const button = this.createFilterButton("category", Category[category]);
+            filters.appendChild(button);
+        }
+
+        const priceFilter = document.createElement("div");
+        priceFilter.classList.add("filter-label");
+        priceFilter.innerText = "Price:";
+        filters.appendChild(priceFilter);
+
+        for (let bracket in PriceBrackets) {
+            const bracketButton = this.createFilterButton("bracket", PriceBrackets[bracket]);
+            filters.appendChild(bracketButton);
+        }
+
+        // TODO: search bar
+
+        this.renderMarketplace();
+
+        this.container.appendChild(this.marketplace);
+
+        return this.container;
+    }
+
+    renderMarketplace() {
+        // empty marketplace
+        this.marketplace.innerHTML = "";
         // render products list
         for (let i = this.start; i < this.end && i < this.prodlist.length; i++) {
             // TODO: Classes for CSS integration
@@ -100,44 +159,90 @@ export class MarketplacePage extends BaseComponent {
             this.marketplace.appendChild(curProduct);
         }
 
-        this.container.appendChild(this.marketplace);
+        if (this.prodlist.length > 0) {
+            // render page buttons and page number
+            const nextPage = document.createElement('button');
+            nextPage.classList.add('page-button');
+            nextPage.id = 'next-page-button';
+            nextPage.textContent = "Next";
+            nextPage.addEventListener('click', () => {
+                if (this.end < this.prodlist.length - 1) {
+                    this.start += 5;
+                    this.end += 5;
+                    this.renderMarketplace();
+                }
+            })
 
-        // render page buttons and page number
-        const nextPage = document.createElement('button');
-        nextPage.classList.add('page-button');
-        nextPage.id = 'next-page-button';
-        nextPage.addEventListener('click', () => {
-            if (this.end < this.prodlist.length - 1) {
-                this.start += 5;
-                this.end += 5;
-                this.render();
-            }
-        })
+            const pageNumber = document.createElement('p');
+            const endItem = Math.min(this.end, this.prodlist.length);
+            pageNumber.innerText = `${this.start + 1} - ${endItem} / ${this.prodlist.length}`;
 
-        const pageNumber = document.createElement('p');
-        pageNumber.innerText = `${this.start + 1} - ${this.end} / ${this.prodlist.length}`;
-
-        const prevPage = document.createElement('button');
-        prevPage.classList.add('page-button');
-        prevPage.id = 'prev-page-button';
-        prevPage.addEventListener('click', () => {
-            if (this.start >= 5) {
-                this.start -= 5;
-                this.end -= 5;
-                this.render();
-            }
-        })
-
-        this.container.appendChild(nextPage);
-        this.container.appendChild(pageNumber);
-        this.container.appendChild(prevPage);
-
-        return this.container;
+            const prevPage = document.createElement('button');
+            prevPage.classList.add('page-button');
+            prevPage.id = 'prev-page-button';
+            prevPage.textContent = "Previous";
+            prevPage.addEventListener('click', () => {
+                if (this.start >= 5) {
+                    this.start -= 5;
+                    this.end -= 5;
+                    this.renderMarketplace();
+                }
+            })
+            this.marketplace.appendChild(nextPage);
+            this.marketplace.appendChild(pageNumber);
+            this.marketplace.appendChild(prevPage);
+        } else {
+            const noItemsFound = document.createElement("p");
+            noItemsFound.innerText = "No Items Found";
+            this.marketplace.appendChild(noItemsFound);
+        }
     }
 
-    // sort and filter
-    // TODO: future feature
+    createFilterButton(kind, text) {
+        const button = document.createElement("button");
+        button.classList.add("filter-button");
+        button.textContent = text;
 
-    // browse categories
-    // TODO: future feature
+        button.addEventListener("click", () => {
+            const toggle = kind === "category" ? this.curCategory : this.curBracket;
+            console.log("button clicked!")
+            if (toggle !== text) {
+                // update toggle
+                if (kind === "category") { // i wish JS had pointers *sad C programmer noises*
+                    this.curCategory = text;
+                } else {
+                    this.curBracket = text;
+                }
+                this.reloadFilters();
+                this.applyFilter((e) => e[kind] === text);
+                // TODO: add styling
+            } else {
+                // update toggle
+                if (kind === "category") {
+                    this.curCategory = "All";
+                } else {
+                    this.curBracket = "All";
+                }
+                this.reloadFilters();
+            }
+            this.renderMarketplace();
+        });
+        return button;
+    }
+
+    applyFilter(cond) {
+        this.prodlist = this.prodlist.filter(cond);
+    }
+
+    reloadFilters() {
+        this.prodlist = this.fullProdList;
+        if (this.curCategory !== "All") {
+            this.applyFilter((e) => e.category === this.curCategory);
+        }
+        if (this.curBracket !== "All") {
+            this.applyFilter((e) => e.bracket === this.curBracket);
+        }
+    }
+
+    // TODO: CSS
 }
---
+++ frontend/main/components/shared/Category.js
@@ -0,0 +1,8 @@
+export const Category = {
+    All: "All",
+    Tanks: "Tanks",
+    Plants: "Plants",
+    Rocks: "Rocks",
+    Fish: "Fish",
+    Misc: "Misc."
+}
