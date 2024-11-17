24b923b02ad399d268ac2fbddf5aa1fa12de0b6e
Winifred Nauda <winifrednauda@gmail.com>
2024-11-17 17:11:34 -0500

    Complete sell products page - make it display only products the current seller is selling - remove no items found text

--- frontend/main/components/MarketplacePage/SellProductsPage.js
+++ frontend/main/components/MarketplacePage/SellProductsPage.js
@@ -1,13 +1,31 @@
 import { MarketplacePage } from "./MarketplacePage.js";
+import { products } from "./Products.js";
 
 export class SellProductsPage extends MarketplacePage {
     constructor() {
         super();
+        this.curSeller = 7905; // to be changed
         this.pageLength = 4, this.end = this.start + this.pageLength;
     }
 
+    getProdList() {
+        const list = products.filter(e => e.sellerid === this.curSeller);
+        
+        for (let i = 0; i < list.length; i++) {
+            list[i].bracket = this.calculateBracket(list[i].price);
+        }
+ 
+        return list;
+    }
+
     render() {
+        this.container.innerHTML = "";
+
+        this.fullProdList = this.getProdList();
+        this.prodList = this.fullProdList;
+
         this.container = super.render();
+
         const addItem = document.createElement('div');
         addItem.classList.add("add-item")
         
@@ -18,7 +36,7 @@ export class SellProductsPage extends MarketplacePage {
 
         const addItemIcon = document.createElement('img');
         addItemIcon.classList.add("add-item-icon");
-        addItemIcon.src = "/frontend/assets/plus-icon.png";
+        addItemIcon.src = "/frontend/assets/plus-icon.svg";
         addItem.appendChild(addItemIcon);
 
         addItem.addEventListener("click", () => {
@@ -26,6 +44,11 @@ export class SellProductsPage extends MarketplacePage {
         })
 
         this.marketplace.childNodes[0].appendChild(addItem);
+        
+        if (this.prodList.length < 1) { // remove no items found text
+            this.marketplace.childNodes[1].innerHTML = "";
+        }
+
         return this.container;
     }
 
@@ -33,7 +56,7 @@ export class SellProductsPage extends MarketplacePage {
         const product = super.createProduct(prodListItem);
         const editButton = document.createElement('img');
         editButton.classList.add("edit-button");
-        editButton.src = "/frontend/assets/edit-icon.png";
+        editButton.src = "/frontend/assets/edit-icon.svg";
         editButton.addEventListener("click", () => {
             console.log(`*takes you to the product page for product ${prodListItem.prodid}*`);
         });
