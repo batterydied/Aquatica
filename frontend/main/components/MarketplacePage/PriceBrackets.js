b4186fdb5541189baf3b2ed1feed2183ecf3885b
Devin Bowler <83326036+devinbowler@users.noreply.github.com>
2024-11-17 21:58:08 -0600

    Delete frontend/main/components/MarketplacePage directory
    Don't know why this is on my branch.

--- frontend/main/components/MarketplacePage/PriceBrackets.js
+++
@@ -1,8 +0,0 @@
-export const PriceBrackets = {
-    bracket1: "Under $20",
-    bracket2: "$20 - $50",
-    bracket3: "$50 - $100",
-    bracket4: "$100 - $500",
-    bracket5: "$500 - $1000",
-    bracket6: "Over $1000"
-}
--- frontend/main/components/MarketplacePage/Sorts.js
+++
@@ -1,5 +0,0 @@
-export const Sorts = {
-    Good: "Highest Rated",
-    Expensive: "Price (High - Low)",
-    Cheap: "Price (Low - High)",
-}
