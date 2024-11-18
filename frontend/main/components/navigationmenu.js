1143f8e9c9b9add8189e89587c0f243f51efde80
HaiyiCai <haiyicai@umass.edu>
2024-11-17 20:25:04 -0500

    Made basic design for navigation menu including a logo svg.Link and Responsive in progress.

---
+++ frontend/main/components/navigationmenu.js
@@ -0,0 +1,18 @@
+// Function to redirect user to a specific page
+// __FIX_ME__USE CLASS
+  
+  // Event Listeners
+  document.addEventListener("DOMContentLoaded", () => {
+    const sellButton = document.querySelector(".sell");
+    const saveListButton = document.querySelector(".save-list");
+    const cartButton = document.querySelector(".cart");
+    const profileButton = document.querySelector(".profile");
+    const logoButton = document.querySelector(".logo-1");
+  
+    // sellButton.addEventListener("click", () => navigateTo("product.html"));
+    // saveListButton.addEventListener("click", () => navigateTo("product.html"));
+    // cartButton.addEventListener("click", () => navigateTo("product.html"));
+    // profileButton.addEventListener("click", () => navigateTo("product.html"));
+    // logoButton.addEventListener("click", () => navigateTo("product.html"));
+  });
+  
