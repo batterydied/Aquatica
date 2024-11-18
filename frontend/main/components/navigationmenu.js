6b4de9b95524fb1032619fbe91e69632eebeb230
HaiyiCai <haiyicai@umass.edu>
2024-11-17 23:55:26 -0500

    Uploaded SVG assets for navigation menu, and updated html. Still in progress for Responsive and Link.

--- frontend/main/components/navigationmenu.js
+++ frontend/main/components/navigationmenu.js
@@ -1,18 +1,29 @@
-// Function to redirect user to a specific page
-// __FIX_ME__USE CLASS
-  
-  // Event Listeners
-  document.addEventListener("DOMContentLoaded", () => {
-    const sellButton = document.querySelector(".sell");
-    const saveListButton = document.querySelector(".save-list");
-    const cartButton = document.querySelector(".cart");
-    const profileButton = document.querySelector(".profile");
-    const logoButton = document.querySelector(".logo-1");
-  
-    // sellButton.addEventListener("click", () => navigateTo("product.html"));
-    // saveListButton.addEventListener("click", () => navigateTo("product.html"));
-    // cartButton.addEventListener("click", () => navigateTo("product.html"));
-    // profileButton.addEventListener("click", () => navigateTo("product.html"));
-    // logoButton.addEventListener("click", () => navigateTo("product.html"));
-  });
+// // Function to redirect user to a specific page
+// const header = document.createElement("header");
+// header.classList.add("header");
+    
+// const logo = document.createElement("div");
+// logo.classList.add("logo");
+// logo.innerText = "Aquatica"; 
+// header.appendChild(logo);
+
+// const nav = document.createElement("nav");
+// nav.classList.add("nav");
+
+// const links = [
+//   //_FIX_ME_Link Pages here:
+//     { text: "Sell", href: "#" }, 
+//     { text: "Virtual Cart", href: "#" },
+//     { text: "Save for later", href: "#" },
+//     { text: "User Center", href: "#" },
+// ];
+// links.forEach(linkData => {
+//     const link = document.createElement("a");
+//     link.href = linkData.href;
+//     link.innerText = linkData.text;
+//     nav.appendChild(link);
+// });
+
+// header.appendChild(nav);
+// this.container.appendChild(header);
   
