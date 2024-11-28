e0ecc18304704ae8e5d0491178b6f146062b86d2
Benson Zheng <bensonzheng2003@gmail.com>
2024-11-27 20:04:11 -0500

    Setup for '/' route [Issue #76]: Implemented '/' route and installed necessary npm packages. Why: Added the root route to serve the frontend and installed essential packages like express, cors, dotenv, path, and url to ensure proper functionality of the Express server.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { handleGlobalError } from './utils/ErrorHandler.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const root = path.dirname(__dirname);
+
 // import AuthRoutes from './routes/AuthRoutes.js';
 // import CartRoutes from './routes/CartRoutes.js';
 // import OrderRoutes from './routes/OrderRoutes.js';
@@ -30,7 +37,7 @@ class Server {
     }));
 
     // Serve static files
-    this.app.use(express.static("../frontend/main"));
+    this.app.use(express.static(path.join(root, 'frontend')));
   }
 
   // Setup routes
@@ -48,6 +55,9 @@ class Server {
   start(port = process.env.PORT || 3000) {
     this.app.listen(port, () => {
       console.log(`Server started on port ${port}`);
+    });
+    this.app.get('/', (req, res) => {
+      res.sendFile(path.join(root, 'frontend', 'index.html'));
     });
   }
 }
