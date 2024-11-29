e2716e48264512dc6531873a985b2218a0b5a03f
Devin W Bowler <devinbowler@gmail.com>
2024-11-29 10:50:09 -0500

    <CartModel> <Create CartModel.js>: <Added the logic for the SQLite storage, so the async methods that will get, save, initialize, remove, or saveforlater within SQLite. Also setting up the Cart data model template.><This is done because the controller will make a request for the data weather to add or get somthing and we need methods and a data saving template in order to keep in line with the database.sqlite.>

--- backend/controllers/CartController.js
+++ backend/controllers/CartController.js
@@ -1,1 +1,41 @@
 // CartController
+import CartModel from "../models/CartModel.js";
+
+class CartController {
+  construstor() {
+    CartModel.getModel().then((model) => {
+      this.model = model;
+    });
+  }
+
+  async getCartItems(req, res) {
+    const items = await CartModel.getCartItems();
+    res.json({ items });
+  }
+
+  async addCartItem(req, res) {
+    const item = req.body;
+    const newItem = await CartModel.addCartItem(item);
+    res.status(201).json(newItem);
+  }
+
+  async removeCartIem(req, res) {
+    const itemId = req.params.id;
+    await CartModel.removeCartIem(itemId);:w
+    res.status(200).send();
+  }
+
+    async saveForLater(req, res) {
+    const itemId = req.params.id;
+    const savedItem = await CartModel.saveForLater(itemId);
+    res.status(201).json(savedItem);
+  }
+
+  async moveToCart(req, res) {
+    const itemId = req.params.id;
+    const movedItem = await CartModel.moveToCart(itemId);
+    res.status(201).json(movedItem);
+  }
+}
+
+export default new CartController();
--- backend/models/CartModel.js
+++ backend/models/CartModel.js
@@ -1,1 +1,50 @@
 // CartModel
+import { Sequelize, DataTypes } from "sequelize";
+
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "database.sqlite",
+});
+
+const Cart = sequelize.define("Cart", {
+  id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+  productId: { type: DataTypes.STRING, allowNull: false },
+  quantity: { type: DataTypes.INTEGER, defaultValue: 1 },
+  isSaved: { type: DataTypes.BOOLEAN, defaultValue: false },
+});
+
+class CartModel {
+  async init() {
+    await sequelize.sync();
+  }
+
+  async getCartItems() {
+    return await Cart.findAll({ where: { isSaved: false } });
+  }
+
+  async addCartItem(item) {
+    return await Cart.create(item);
+  }
+
+  async removeCartItem(itemId) {
+    return await Cart.destroy({ where: { id: itemId } });
+  }
+
+  async saveForLater(itemId) {
+    const item = await Cart.findByPk(itemId);
+    item.isSaved = true;
+    await item.save();
+    return item;
+  }
+
+  async moveToCart(itemId) {
+    const item = await Cart.findByPk(itemId);
+    item.isSaved = false;
+    await item.save();
+    return item;
+  }
+}
+
+export default new CartModel();
+
