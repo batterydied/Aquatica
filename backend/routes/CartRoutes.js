fa0c27d58c0b289e37a17731a211b52bc4a9b1d0
Devin W Bowler <devinbowler@gmail.com>
2024-11-29 10:46:39 -0500

    <CartRoutes> <Create CartRoutes.js>: <The CartRoutes have been added to redirect any frontend requests to the CartController so that it can interact and send information from the backend, to the frontend.><These routes needed to be setup so that when in my services I eventually setup api requests, those requests will go to this file, and know what to do in the controller for sending back information or recieving and storing it.>

--- backend/routes/CartRoutes.js
+++ backend/routes/CartRoutes.js
@@ -1,1 +1,28 @@
 // CartRoutes
+import express from "express";
+import CartController from "../controllers/CartController.js";
+
+class CartRoutes {
+  constructor(){
+    this.router = express.Router();
+    this.initializeRoutes();
+  }
+
+  initializeRoutes() {
+    // Cart-related endpoints
+    router.get("/cart", async (req, res) => await CartController.getCartItems(req, res));
+    router.post("/cart", async (req, res) => await CartController.addCartItem(req, res));
+    router.delete("cart/:id" async (req, res) => await CartController.removeCartItem(req, res));
+
+    // Save for later endpoints
+    router.post("/cart/save/:id", async (req, res) => await CartController.saveForLater(req, res));
+    router.post("/cart/move/:id", async (req, res) => await CartController.moveToCart(req, res));
+
+  }
+
+  getRouter(){
+    return this.router;
+  }
+}
+
+export default new CartRoutes().getRouter();
