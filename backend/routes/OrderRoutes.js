f177795fc2734c1466b5edccccd171e9629d693d
Devin W Bowler <devinbowler@gmail.com>
2024-12-02 09:08:14 -0500

    <OrderRoutes><Create OrderRoutes.js #52> : <Added the inital structure to the cart routes page ensuring the proper routes to the controllers is setup, which include just a fetch all and add route.><I did this so that later on requests from the frontend are easier to manage.>

--- backend/routes/OrderRoutes.js
+++ backend/routes/OrderRoutes.js
@@ -1,1 +1,33 @@
 // OrderRoutes
+
+import express from "express";
+import OrderController from "../controllers/OrderController.js";
+
+class OrderRoutes {
+  constructor() {
+    this.router = express.Router(); // Properly initialize the router
+    this.initializeRoutes();
+  }
+
+initializeRoutes() {
+    // Order endpoints
+  
+    // Get all orders
+    this.router.get("/", (req, res) => {
+        console.log("GET /api/order called.");
+        CartController.getOrders(req, res);
+    });
+
+    // Make a new order
+    this.router.post("/", (req, res) => {
+        console.log("POST /api/order called.");
+        OrderController.addOrder(req, res);
+    });  
+}
+
+  getRouter() {
+    return this.router; // Return the router instance
+  }
+}
+
+export default new OrderRoutes().getRouter();
