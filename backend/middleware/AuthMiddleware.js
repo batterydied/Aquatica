516c5e194d6184f93e306779462ac4d5f8e3d431
HaiyiCai <haiyicai@umass.edu>
2024-12-03 10:36:17 -0500

    AuthMiddleware #73: Changed import format for consistency. Fixed bug of wrong method name.

--- backend/middleware/AuthMiddleware.js
+++ backend/middleware/AuthMiddleware.js
@@ -9,40 +9,45 @@
 /* Works with UserModel.js to retrieve and validate user data as needed. It's used in routes that 
 require user authentication: profile updates, placing orders, accessing user-specific data, etc. 
 */
-const jwt = require('jsonwebtoken');
-const UserModel = require('../models/UserModel');
+import jwt from "jsonwebtoken";
+import UserModel from "../models/UserModel.js";
 
 // Validates user credentials by checking the authentication token or session associated with the incoming request.
 const authMiddleware = async (req, res, next) => {
-/* 1. Token Validation:
+  /* 1. Token Validation:
   - Verifies if a token is present in the Authorization header of the request.
   - Decodes and validates the token to ensure it is valid, not expired, and issued by the server.
-*/
+  */
   const token = req.headers.authorization; // TODO Assuming the token is sent in the Authorization header as "Bearer <token>"
+      // const token = req.header("authorization")?.split(" ")[1];
   if (!token) {
-      return res.status(401).json({ error: 'Unauthorized: No token provided, access denied.' });
+      return res.status(401).json({ error: 'Unauthenticated: No token provided, access denied.' });
   }
 
   try {
     // Verify the token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-/* 2. Attach User Info:
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+  /* 2. Attach User Info:
   - Extracts user details from the token.
   - Attaches them to the request object for downstream use.
-*/
-    const user = await UserModel.findById(decoded.userId);
+  */
+    const user = await UserModel.getUserById(decodedToken.userId);
     if (!user) {
-        return res.status(401).json({ error: 'Unauthorized: User not found, access denied.' });
+      return res.status(401).json({ error: 'Unauthorized: User not found, access denied.' });
     }
+    if (user.tokenVersion != decodedToken.tokenVersion){
+      return res.status(401).json({ error: 'Unauthorized: Token is invalid, Please log in again.' });
+    }
+
     req.user = user;    // Attach user to the request object
     next();             // Proceed to the next middleware or route handler
-/*3. Error Handling:
+  } catch (error) {
+  /*3. Error Handling:
   - Responds with a 401_Unauthorized error if the token is missing or invalid.
   - Responds with a 403_Forbidden error if access is denied for any other reason.
-*/
-  } catch (error) {
-    console.error('Authentication error:', error);
-    return res.status(403).json({ error: 'Unauthorized: Invalid or expired token, access denied.' });
+  */
+    console.error("Error in RoleMiddleware:", error);
+    res.status(500).json({error:"An unexpected error occurred while verifying roles."});
   }
 };
 
